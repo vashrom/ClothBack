3e64f6d99a91dba3d2574e8da89517824f01401a
vashrom <you@example.com>
2020-06-16 18:10:33 +0300

    Intstall dependencies and connect to DB

---
+++ config/helpers.js
@@ -0,0 +1,16 @@
+const MySqli = require('mysqli');
+
+let conn = new MySqli({
+    host: '127.0.0.1',
+    post: 3306,
+    user: 'root',
+    pass: '',
+    db: 'mega_shop'
+
+});
+
+let db = conn.emit(false, '');
+
+module.exports = {
+    database: db
+};
