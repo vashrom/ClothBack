57f41d09f8e851325684e7e0b4bf0f1205990f35
vashrom <50840159+vashrom@users.noreply.github.com>
2021-01-30 19:45:47 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const db = {}
-const sequelize = new Sequelize("macaron", "root", "", {
-    host: "127.0.0.1",
+const sequelize = new Sequelize("macaron", "vasko", "", {
+    host: "45.94.158.114",
     dialect: "mysql",
     operatorsAliases: false,
 
